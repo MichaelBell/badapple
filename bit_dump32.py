9446b44e30fbf8ba9b20570bc0f3340b7e32982b
Mike Bell <mdb036@gmail.com>
2024-11-23 00:23:27 +0000

    Presto version

--- bit_dump32.py
+++ bit_dump32.py
@@ -3,60 +3,33 @@
 import struct
 from PIL import Image
 
-out_file = open("badapple640x480-32m.bin", "wb")
-
-def repeat_n_times(list, n):
-    return [item for item in list for i in range(n)]
-
-TWO  = repeat_n_times((0, 0x3C), 32)
-FOUR = repeat_n_times((0, 0x14, 0x28, 0x3C), 16)
-EIGHT= repeat_n_times((0, 0x08, 0x10, 0x18, 0x24, 0x2C, 0x34, 0x3C), 8)
-SIXT = repeat_n_times((0, 0x4, 0x8, 0xC, 0x10, 0x14, 0x18, 0x1C, 0x20, 0x24, 0x28, 0x2C, 0x30, 0x34, 0x34, 0x3C), 4)
-THIRT= [0, 0x40, 0x44, 0x48, 0x4, 0x4C, 0x50, 0x54, 0x8, 0x58, 0x5C, 0x60, 0xC, 0x64, 0x68, 0x6C, 0x10, 0x70, 0x74, 0x78, 0x14, 0x14, 0x14, 0x14]
-THIRT.extend(SIXT[24:])
-
-print(len(SIXT))
-print(len(THIRT))
-
-colour_shift_changes = {
-    1: SIXT,
-    1509: FOUR,
-    1526: SIXT,
-    1873: TWO,
-    1913: SIXT,
-    2716: EIGHT,
-    2737: SIXT,
-    2916: EIGHT,
-    2957: THIRT,
-    3320: SIXT
-}
+out_file = open("badapple480x480-565.bin", "wb")
+data_len = 0
 
 for i in range(1,6957):
-    img = Image.open("png_frames/badapple%04d.png" % (i,)).resize((640,480))
+    img = Image.open("png_frames/badapple%04d.png" % (i,)).resize((480,480))
 
     data = img.load()
 
-    if i in colour_shift_changes.keys():
-        colour_shift = colour_shift_changes[i]
-
     for y in range(0,480):
         span_len = 0
-        span_colour = 0
-        for x in range(640):
-            colour = colour_shift[data[x, y][0] >> 2]
+        span_colour = -255
+        for x in range(480):
+            colour = data[x, y][0] >> 5
             if colour != span_colour:
-                #if span_len < 4 and span_colour != 0 and span_colour != 0x1C:
-                #    span_colour = colour
-                #    span_len += 1
-                #else:
                 if span_len > 0:
-                    out_file.write(struct.pack('<H', (span_len << 7) + span_colour))
+                    span_colour = (span_colour << 2) | (span_colour >> 1)
+                    span_colour = (span_colour << 11) | (span_colour << 6) | span_colour
+                    span_colour = (span_colour & 0xFF) << 8 | ((span_colour >> 8) & 0xFF)  # Stupid byte swap
+                    out_file.write(struct.pack('<HH', span_len, span_colour))
+                    data_len += 4
                 span_len = 1
                 span_colour = colour
-            elif span_len == 511:
-                out_file.write(struct.pack('<H', (span_len << 7) + span_colour))
-                span_len = 1
             else:
                 span_len += 1
-        out_file.write(struct.pack('<H', (span_len << 7) + span_colour))
-    print("Frame %d" % (i,))
+        span_colour = (span_colour << 2) | (span_colour >> 1)
+        span_colour = (span_colour << 11) | (span_colour << 6) | span_colour
+        span_colour = (span_colour & 0xFF) << 8 | ((span_colour >> 8) & 0xFF)  # Stupid byte swap
+        out_file.write(struct.pack('<HH', span_len, span_colour))
+        data_len += 4
+    print("Frame %d, len %.2fMB" % (i, data_len / (1024 * 1024)))
--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-#include "libraries/pico_graphics/pico_graphics_dv.hpp"
-#include "drivers/dv_display/dv_display.hpp"
+#include "libraries/pico_graphics/pico_graphics.hpp"
+#include "drivers/st7701/st7701.hpp"
 
 #include "ff.h"
 
-#include "pico/audio_i2s.h"
 #include "pico/multicore.h"
 #include "pico/sync.h"
 
@@ -15,7 +14,20 @@ FATFS fs;
 FIL fil;
 FIL audio_file;
 FRESULT fr;
-DVDisplay display;
+
+#define FRAME_WIDTH 480
+#define FRAME_HEIGHT 480
+static const uint BACKLIGHT = 45;
+static const uint LCD_CLK = 26;
+static const uint LCD_CS = 28;
+static const uint LCD_DAT = 27;
+static const uint LCD_DC = -1;
+static const uint LCD_D0 = 1;
+
+uint16_t frame_buffer[FRAME_WIDTH * FRAME_HEIGHT];
+
+ST7701* presto;
+PicoGraphics_PenRGB565* display;
 
 #define NUM_BUFFERS 16
 #define BUFFER_LEN 256
@@ -24,71 +36,6 @@ uint16_t buf[NUM_BUFFERS][BUFFER_LEN];
 volatile uint write_buf;
 volatile uint read_buf;
 uint buf_idx;
-
-#define DISPLAY_WIDTH 640
-#define DISPLAY_HEIGHT 480
-
-#define FRAME_WIDTH 640
-#define FRAME_HEIGHT 480
-
-#define AUDIO_SAMPLES_PER_BUFFER 128
-
-static struct audio_buffer_pool *init_audio() {
-
-    static audio_format_t audio_format = {
-            .sample_freq = 22050,
-            .format = AUDIO_BUFFER_FORMAT_PCM_S16,
-            .channel_count = 2,
-    };
-
-    static struct audio_buffer_format producer_format = {
-            .format = &audio_format,
-            .sample_stride = 4
-    };
-
-    struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 8, AUDIO_SAMPLES_PER_BUFFER);
-    const struct audio_format *output_format;
-
-    // This is a silly hack around audio i2s not claiming resources.
-    uint8_t audio_channel = dma_claim_unused_channel(true);
-    dma_channel_unclaim(audio_channel);
-    uint8_t audio_sm = pio_claim_unused_sm(pio0, true);
-    pio_sm_unclaim(pio0, audio_sm);
-
-    struct audio_i2s_config config = {
-            .data_pin = PICO_AUDIO_I2S_DATA_PIN,
-            .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
-            .dma_channel = audio_channel,
-            .pio_sm = audio_sm
-    };
-
-    output_format = audio_i2s_setup(&audio_format, &config);
-    if (!output_format) {
-        panic("PicoAudio: Unable to open audio device.\n");
-    }
-
-    return producer_pool;
-}
-
-static void fill_audio_buffer(audio_buffer_pool *producer_pool) {
-    audio_buffer_t *buffer;
-
-    buffer = take_audio_buffer(producer_pool, false);
-    if (!buffer) return;
-
-    uint bytes_read;
-    //mutex_enter_blocking(&fs_mutex);
-    fr = f_read(&audio_file, buffer->buffer->bytes, buffer->max_sample_count * 4, &bytes_read);
-    //mutex_exit(&fs_mutex);
-    if (fr != FR_OK || bytes_read == 0) {
-        printf("Audio read fail\n");
-        return;
-    }
-
-    buffer->sample_count = bytes_read >> 2;
-    give_audio_buffer(producer_pool, buffer);
-    //printf(".");
-}
 
 static void fill_video_buffer() {
     uint next_buf_idx = (write_buf + 1) & 0xF;
@@ -104,44 +51,20 @@ static void fill_video_buffer() {
     write_buf = next_buf_idx;
 }
 
-#define SHORT_BUF_LEN 32
 static bool display_frame() {
-    //uint8_t short_buffer[SHORT_BUF_LEN] alignas(4);
-
-    for (int y = 0; y < DISPLAY_HEIGHT; ++y)
+    for (int y = 0; y < FRAME_HEIGHT; ++y)
     {
         int x = 0;
-        //int short_buf_idx = 0;
-        //int short_buf_x = 0;
-        while (x < DISPLAY_WIDTH) {
-            const uint16_t span_len = buf[read_buf][buf_idx] >> 7;
-            const uint8_t colour = buf[read_buf][buf_idx] & 0x7C;
-
-#if 0
-            if (span_len < 5) {
-                if (short_buf_idx + span_len > SHORT_BUF_LEN) {
-                    display.write_palette_pixel_span({short_buf_x, y}, short_buf_idx, short_buffer);
-                    short_buf_idx = 0;
-                }
-                if (short_buf_idx == 0) short_buf_x = x;
-                for (int i = 0; i < span_len; ++i) {
-                    short_buffer[short_buf_idx++] = colour;
-                }
-            }
-            else {
-                if (short_buf_idx != 0) {
-                    display.write_palette_pixel_span({short_buf_x, y}, short_buf_idx, short_buffer);
-                    short_buf_idx = 0;
-                }
-                display.write_palette_pixel_span({x, y}, span_len, colour);
-            }
-#else
-            display.write_palette_pixel_span({x, y}, span_len, colour);
-#endif
+        while (x < FRAME_WIDTH) {
+            const uint16_t span_len = buf[read_buf][buf_idx];
+            const uint16_t colour = buf[read_buf][buf_idx+1];
+            display->set_pen(colour);
+            display->set_pixel_span({x, y}, span_len);
 
             x += span_len;
 
-            if (++buf_idx == BUFFER_LEN) {
+            buf_idx += 2;
+            if (buf_idx == BUFFER_LEN) {
                 uint next_buf_idx = (read_buf + 1) & 0xF;
                 while (next_buf_idx == write_buf);
                 read_buf = next_buf_idx;
@@ -149,12 +72,6 @@ static bool display_frame() {
                 buf_idx = 0;
             }
         }
-#if 0
-        if (short_buf_idx != 0) {
-            display.write_palette_pixel_span({short_buf_x, y}, short_buf_idx, short_buffer);
-            short_buf_idx = 0;
-        }
-#endif
     }
 
     return true;
@@ -163,16 +80,10 @@ static bool display_frame() {
 volatile bool run_fs = false;
 
 void core1_main() {
-    audio_buffer_pool *ap = init_audio();
-    bool ok = audio_i2s_connect(ap);
-    assert(ok);
-    audio_i2s_set_enabled(true);
-
     while (true) {
         multicore_fifo_pop_blocking();
         
         while (run_fs) {
-            fill_audio_buffer(ap);
             fill_video_buffer();
         }
 
@@ -181,8 +92,12 @@ void core1_main() {
 }
 
 int main() {
-    set_sys_clock_khz(168000, true);
+    set_sys_clock_khz(240000, true);
     stdio_init_all();
+
+    gpio_init(LCD_CS);
+    gpio_put(LCD_CS, 1);
+    gpio_set_dir(LCD_CS, 1);
 
     sleep_ms(5000);
     printf("Hello\n");
@@ -193,40 +108,22 @@ int main() {
       return 0;
     }
 
-    DVDisplay::preinit();
+    presto = new ST7701(FRAME_WIDTH, FRAME_HEIGHT, ROTATE_0, SPIPins{spi1, LCD_CS, LCD_CLK, LCD_DAT, PIN_UNUSED, LCD_DC, BACKLIGHT}, frame_buffer);
+    display = new PicoGraphics_PenRGB565(FRAME_WIDTH, FRAME_HEIGHT, frame_buffer);
 
-    display.init(DISPLAY_WIDTH, DISPLAY_HEIGHT, DVDisplay::MODE_PALETTE, FRAME_WIDTH, FRAME_HEIGHT);
-    PicoGraphics_PenDV_P5 graphics(FRAME_WIDTH, FRAME_HEIGHT, display);
+    presto->init();
 
     multicore_launch_core1(core1_main);
     printf("Init\n");
 
-    for (int i = 0; i < 16; ++i) {
-        //uint8_t col = (i << 2) | (i << 5) | (i >> 1);
-        uint8_t col = i | (i << 4);
-        //uint8_t col = (i >> 2) | (i << 3);
-        graphics.create_pen(col, col, col);
-    }
-    for (int i = 1; i < 20; ++i) {
-        if ((i & 3) == 0) continue;
-        uint8_t col = (i >> 4) | (i << 2);
-        graphics.create_pen(col, col, col);
-    }
-
-    graphics.set_pen(0);
-    graphics.clear();
-    display.flip();
+    display->set_pen(0);
+    display->clear();
+    //presto->update(display);
 
     while (true) {
-        fr = f_open(&fil, "/badapple640x480-32m.bin", FA_READ);
+        fr = f_open(&fil, "/badapple480x480-565.bin", FA_READ);
         if (fr != FR_OK) {
             printf("Failed to open badapple video, error: %d\n", fr);
-            return 0;
-        }
-
-        fr = f_open(&audio_file, "/badapple-22050.pcm", FA_READ);
-        if (fr != FR_OK) {
-            printf("Failed to open badapple audio, error: %d\n", fr);
             return 0;
         }
 
@@ -241,7 +138,6 @@ int main() {
         read_buf = 0;
         buf_idx = 0;
 
-        printf("Audio start\n");
         run_fs = true;
         multicore_fifo_push_blocking(0);
 
@@ -256,7 +152,7 @@ int main() {
             else {
                 printf("Frame %d time %lldms\n", i, absolute_time_diff_us(start_time, get_absolute_time()) / 1000);
             }
-            display.flip();
+            //presto->update(display);
             start_time = delayed_by_ms(start_time, (i % 3 == 2) ? 34 : 33);
         }
         run_fs = false;
